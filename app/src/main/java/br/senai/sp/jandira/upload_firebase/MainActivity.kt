c66e4e676d024218ef48562555dba56a706c1759
MÔNICA RIBEIRO DA SILVA <22282167@dsn127m1241955.cfp127aluno.local>
2023-09-22 11:12:18 -0300

    upload de imagens

--- app/src/main/java/br/senai/sp/jandira/upload_firebase/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/upload_firebase/MainActivity.kt
@@ -3,7 +3,11 @@ package br.senai.sp.jandira.upload_firebase
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import br.senai.sp.jandira.upload_firebase.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
@@ -15,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 //  STORAGEREFERENCE PERMITE A MANIPULAÇÃO DO CLOUD STORE(ARMAZENA ARQUIVOS)
     private lateinit var storageref: StorageReference
 //  FIREBASEFIRESTORE - PERMITE A MANIPULAÇÃO DO BANCO DE DADOS NOSQL
-    private lateinit var firebaseFireStorage: FirebaseStorage
+    private lateinit var firebaseFireStorage: FirebaseFirestore
 
 //  URI - PERMITE A MANIPULAÇÃO DE ARQUIVOSATRAVÉS DO SEU ENDEREÇAMENTO
     private var imageUri: Uri? = null
@@ -24,6 +28,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initiVars()
+        registerClickEvents()
+    }
+
+//    Funçao de inicialização dos recursos do firebase
+    private fun initiVars(){
+        storageref = FirebaseStorage.getInstance().reference.child("imagens")
+        firebaseFireStorage = FirebaseFirestore.getInstance()
+    }
+
+//    Funçao para o lancador de recuperação de imagens da galeria
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
+        imageUri = it
+        binding.imageView.setImageURI(it)
+    }
+
+    private fun registerClickEvents(){
+//      trata o evento de click do componente imageview
+        binding.imageView.setOnClickListener{
+            resultLauncher.launch("image/*")
+
+//          trata o evento de click do botao upload
+            binding.uploadBtn.setOnClickListener{
+                uploadImage()
+            }
+        }
+    }
+//  Função de upload
+    private fun uploadImage(){
+
+    binding.progressBar.visibility = View.VISIBLE
+//    Define um nome unico para a imagem com uso de um valor timestamp
+        storageref = storageref.child(System.currentTimeMillis().toString())
+
+        imageUri?.let{
+            storageref.putFile(it).addOnCompleteListener{
+                task->
+                    if (task.isSuccessful){
+                        Toast.makeText(this,"UPLOAD CONCLUIDO!", Toast.LENGTH_LONG).show()
+
+                    } else {
+                        Toast.makeText(this,"ERRO AO REALIZAR O UPLOAD!", Toast.LENGTH_LONG).show()
+                    }
+
+                    binding.progressBar.visibility = View.GONE
+            }
+        }
     }
 }
